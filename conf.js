472be4a3140a9c921fb074a2d282bb9ea406f62c
Yinziming <21014810+yinziminganion@users.noreply.github.com>
2020-02-12 21:53:46 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '110',
+  ver: '111',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,20 +12,20 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
+    //'demo-hk': {
+    //  label: '演示服务-香港节点',
+    //  lines: {
+    //    // 主机:权重
+    //    'node-aliyun-hk-1.etherdream.com:8443': 1,
+    //    'node-aliyun-hk-2.etherdream.com:8443': 2,
+    //  }
+    //},
+    //'demo-sg': {
+    //  label: '演示服务-新加坡节点',
+    //  lines: {
+    //    'node-aliyun-sg.etherdream.com:8443': 1,
+    //  },
+    //},
     'mysite': {
       label: '当前站点',
       lines: {
@@ -38,14 +38,15 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
-
+        //'node-cfworker-2.etherdream.com': 4,
+		
+        'fq.yinziming.workers.dev': 1,
+		
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        //'b.hehe.workers.dev': 1,
+        //'b.lulu.workers.dev': 1,
+        //'b.jsproxy.workers.dev': 1,
       }
     }
   },
